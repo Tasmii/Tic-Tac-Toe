261a03f1f5cbca9132324013ec6981d50979849e
Tasmii <ilahitasmiya@gmail.com>
2024-06-25 11:26:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import TicTacToe from './components/TicTacToe'
+
+function App() {
+  return (
+    <TicTacToe/>
+  )
+}
+
+export default App
